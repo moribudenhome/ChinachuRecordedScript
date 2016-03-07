78a22f88c743dfa94fcb9fa02c0311cc0db94eb4
mop <moribuden.mop@gmail.com>
2016-03-07 23:36:43 +0900

    EncodeWaitingモデル追加

---
+++ windows_side/TsEncode/TsEncode/Model/EncodeWaitings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model
+{
+	public class EncodeWaitings
+	{
+		static EncodeWaitings instance = null;
+		private EncodeWaitings() { }
+		public static EncodeWaitings Get()
+		{
+			if ( instance == null ) {
+				instance = new EncodeWaitings();
+			}
+			return instance;
+		}
+
+		public enum ENCODE_STATE
+		{
+			wait = 0,
+			progress,
+			success,
+			failure,
+		}
+
+		public class EncodeWaitingInfo
+		{
+			public EncodeWaitingInfo( int id, string srcPath, string dstPath, ENCODE_STATE encodeState )
+			{
+				Id = id;
+				SrcPath = srcPath.Replace( '/', '\\' );
+				DstPath = dstPath.Replace( '/', '\\' );
+				EncodeState = encodeState;
+			}
+			public int Id { get; private set; }
+			public string SrcPath { get; private set; }
+			public string DstPath { get; private set; }
+			public ENCODE_STATE EncodeState { get; private set; }
+		}
+		List<EncodeWaitingInfo> infos = new List<EncodeWaitingInfo>();
+
+		// エンコード待ちリスト更新
+		public void UpdateEncodeWaitingList()
+		{
+			infos.Clear();
+			MySQLUtility.QueryDictionary( ( d ) => {
+				infos.Add( new EncodeWaitingInfo( (int)d[ "id" ], (string)d[ "src_path" ], (string)d[ "dst_path" ], (ENCODE_STATE)d[ "encode_state" ] ) );
+			}, "SELECT id,src_path,dst_path,encode_state FROM encode_waitings where encode_state={0}", 0 );
+			Console.WriteLine();
+		}
+
+		// エンコード状態更新.
+		public void UpdateEncodeState( int id, ENCODE_STATE state ) 
+		{
+			MySQLUtility.Query( null, "UPDATE encode_waitings SET encode_state={0} WHERE id={1}", (int)state, id );
+			UpdateEncodeWaitingList();
+		}
+	}
+}
+
--- windows_side/TsEncode/TsEncode/Program.cs
+++ windows_side/TsEncode/TsEncode/Program.cs
@@ -10,24 +10,30 @@ namespace TsEncode
 	{
 		static void Main( string[] args )
 		{
-			//PowerShellUtility.Execute( "ps/encode.ps1", new[] { "a", "b", "c" } );
-			//if ( MySQLUtility.Open() ) {
-			//	MySQLUtility.Query( ( reader ) => {
-			//		string[] row = new string[ reader.FieldCount ];
-			//		for ( int i = 0; i < reader.FieldCount; i++ ) {
-			//			Console.WriteLine( reader.GetString( i ) );
-			//		}
-			//	}, "SELECT src_path,dst_path,encode_state FROM encode_waitings where encode_state={0}", 0 );
-			//	Console.WriteLine( "" );
-			//	Console.WriteLine( "" );
-			//	Console.WriteLine( "" );
-			//	MySQLUtility.QueryDictionary( ( dic ) => {
-			//		foreach ( var e in dic ) {
-			//			Console.WriteLine( "key=" + e.Key + " value=" + e.Value );
-			//		}
-			//	}, "SELECT src_path,dst_path,encode_state FROM encode_waitings where encode_state={0}", 0 );
-			//}
+			if( !MySQLUtility.Open() ) { return; }
 
+			//model.EncodeWaitings.Get().UpdateEncodeWaitingList();
+			//model.EncodeWaitings.Get().UpdateEncodeState( 1, model.EncodeWaitings.ENCODE_STATE.wait );
+
+			MySQLUtility.Close();
+			//PowerShellUtility.Execute( "ps/encode.ps1", new[] { "a", "b", "c" } );
+			if ( MySQLUtility.Open() ) {
+				//MySQLUtility.Query( ( reader ) => {
+				//	string[] row = new string[ reader.FieldCount ];
+				//	for ( int i = 0; i < reader.FieldCount; i++ ) {
+				//		Console.WriteLine( reader.GetString( i ) );
+				//	}
+				//}, "SELECT src_path,dst_path,encode_state FROM encode_waitings where encode_state={0}", 0 );
+				//Console.WriteLine( "" );
+				//Console.WriteLine( "" );
+				//Console.WriteLine( "" );
+				//MySQLUtility.QueryDictionary( ( d ) => {
+				//	//foreach ( var e in dic ) {
+				//	//	Console.WriteLine( "key=" + e.Key + " value=" + e.Value );
+				//	//}
+				//	Console.WriteLine( (int)d[ "encode_state" ] );
+				//}, "SELECT src_path,dst_path,encode_state FROM encode_waitings where encode_state={0}", 0 );
+			}
 		}
 	}
 }
