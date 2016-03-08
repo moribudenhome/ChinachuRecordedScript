8471c00257dfb0de17830d6b4935389096afd320
mop <moribuden.mop@gmail.com>
2016-03-08 23:51:16 +0900

    PowerShell実行時にバッチの進行状況を標準出力へ出すようにした

--- windows_side/TsEncode/TsEncode/Utility/PowerShellUtility.cs
+++ windows_side/TsEncode/TsEncode/Utility/PowerShellUtility.cs
@@ -4,34 +4,47 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using System.Text;
 using System.Threading.Tasks;
 
 public class PowerShellUtility
 {
+	static PSDataCollection<PSObject> outputCollection = null;
+	
 	// 指定されたPowerShellを実行
-	public static void Execute( string filepath, IEnumerable input )
+	public static void Execute( string filepath, string[] input )
 	{
 		string shell = "";
 		try {
 			using ( StreamReader sr = new StreamReader( filepath, Encoding.GetEncoding( "Shift_JIS" ) ) )  {
 				shell = sr.ReadToEnd(); 
 			}
-			Console.WriteLine( shell );
 
 		} catch ( Exception e ) {
 			Console.WriteLine( e.Message );
 		}
 
-		using ( var invoker = new RunspaceInvoke() ) {
-			// TODO ブロッキングしないようにしたい.
-			// TODO エラーハンドル.
-			var results = invoker.Invoke( shell, input );
+		outputCollection = new PSDataCollection<PSObject>();
+		outputCollection.DataAdded += OnDataAdded;
 
-			foreach ( var result in results ) {
-				Console.WriteLine( result );
+		using ( var rs = RunspaceFactory.CreateRunspace() ) {
+			rs.Open();
+			using ( PowerShell ps = PowerShell.Create() ) {
+				ps.AddScript( shell );
+				for ( int i = 0; i < input.Length; i++ ) {
+					ps.AddParameter( "arg" + i, input[ i ] );
+				}
+				IAsyncResult a = ps.BeginInvoke<PSObject, PSObject>( null, outputCollection );
+				a.AsyncWaitHandle.WaitOne();
 			}
 		}
+	}
+
+	static void OnDataAdded( object sender, DataAddedEventArgs e )
+	{
+		if ( outputCollection == null ) { return; }
+		Console.WriteLine( outputCollection[ e.Index ] );
 	}
 }
 
--- windows_side/TsEncode/TsEncode/ps/encode.ps1
+++ windows_side/TsEncode/TsEncode/ps/encode.ps1
@@ -4,22 +4,9 @@
 # 引数分解 後で引数パースを配列にしよう
 # 引数: sambaのIP, 元ファイルパス, 出力先ファイルパス  
 
-$count = 0
-foreach($x in $input)
-{
-  switch ($count)
-  {
-    0 { $arg1 = $x }
-    1 { $arg2 = $x }
-	2 { $arg3 = $x }
-  }
-  $count++
-}
+param($arg0,$arg1,$arg2)
 
-echo $arg1 $arg2 $arg1
-
-cd \\$arg1\share
-#HandBrakeCLI.exe -i "$arg2" -o "$arg3" -f mp4 -e x264 --x264-preset=medium --x264-tune=animation --h264-profile=high --h264-level="4.1" -q 21 -r auto --vfr -a 1 -E fdk_aac -6 auto -R auto -w 1280 -l 720 --crop 0:0:0:0 --decomb --detelecine -P;
-HandBrakeCLI --input "$arg2" --encoder x264 --x264-preset medium --x264-tune animation --quality 21 --encopts 'vbv-maxrate=26000:vbv-bufsize=22000' --rate 29.97 --ab 128 --deinterlace medium --output "$arg3"
-
+cd \\$arg0\share
+#HandBrakeCLI.exe -i "$arg1" -o "$arg2" -f mp4 -e x264 --x264-preset=medium --x264-tune=animation --h264-profile=high --h264-level="4.1" -q 21 -r auto --vfr -a 1 -E fdk_aac -6 auto -R auto -w 1280 -l 720 --crop 0:0:0:0 --decomb --detelecine -P;
+HandBrakeCLI --input "$arg1" --encoder x264 --x264-preset medium --x264-tune animation --quality 21 --encopts 'vbv-maxrate=26000:vbv-bufsize=22000' --rate 29.97 --ab 128 --deinterlace medium --output "$arg2"
 
