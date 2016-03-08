921a6985bac2b398b32a959d9febbc9658fe9ca1
mop <moribuden.mop@gmail.com>
2016-03-08 21:12:39 +0900

    若干音ズレして見えたのでエンコードコマンド修正

--- windows_side/TsEncode/TsEncode/ps/encode.ps1
+++ windows_side/TsEncode/TsEncode/ps/encode.ps1
@@ -19,7 +19,7 @@ foreach($x in $input)
 echo $arg1 $arg2 $arg1
 
 cd \\$arg1\share
-HandBrakeCLI.exe -i "$arg2" -o "$arg3" -f mp4 -e x264 --x264-preset=medium --x264-tune=animation --h264-profile=high --h264-level="4.1" -q 21 -r auto --vfr -a 1 -E fdk_aac -6 auto -R auto -w 1280 -l 720 --crop 0:0:0:0 --decomb --detelecine -P;
-
+#HandBrakeCLI.exe -i "$arg2" -o "$arg3" -f mp4 -e x264 --x264-preset=medium --x264-tune=animation --h264-profile=high --h264-level="4.1" -q 21 -r auto --vfr -a 1 -E fdk_aac -6 auto -R auto -w 1280 -l 720 --crop 0:0:0:0 --decomb --detelecine -P;
+HandBrakeCLI --input "$arg2" --encoder x264 --x264-preset medium --x264-tune animation --quality 21 --encopts 'vbv-maxrate=26000:vbv-bufsize=22000' --rate 29.97 --ab 128 --deinterlace medium --output "$arg3"
 
 
