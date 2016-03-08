c7b0c619a176f973a964b8b51264376a930a34fb
mop <moribuden.mop@gmail.com>
2016-03-09 02:42:32 +0900

    エンコードのオプションを修正。音ズレしなくなったきがする

--- windows_side/TsEncode/TsEncode/ps/encode.ps1
+++ windows_side/TsEncode/TsEncode/ps/encode.ps1
@@ -8,5 +8,4 @@ param($arg0,$arg1,$arg2)
 
 cd \\$arg0\share
 #HandBrakeCLI.exe -i "$arg1" -o "$arg2" -f mp4 -e x264 --x264-preset=medium --x264-tune=animation --h264-profile=high --h264-level="4.1" -q 21 -r auto --vfr -a 1 -E fdk_aac -6 auto -R auto -w 1280 -l 720 --crop 0:0:0:0 --decomb --detelecine -P;
-HandBrakeCLI --input "$arg1" --encoder x264 --x264-preset medium --x264-tune animation --quality 21 --encopts 'vbv-maxrate=26000:vbv-bufsize=22000' --rate 29.97 --ab 128 --deinterlace medium --output "$arg2"
-
+HandBrakeCLI -i "$arg1" -o "$arg2" -f mp4 -e x264 --x264-preset slow --x264-tune animation --h264-profile=high -q 21 --cfr -r 29.97 --ab 128 --deinterlace slow
